990cf13bb80d4620d18170b69720b41cd850de12
SaraNiknamHesar <saraniknaam76@gmail.com>
2025-09-17 22:50:07 +0200

    helpers_functions

--- functions/helpers.php
+++ functions/helpers.php
@@ -3,7 +3,20 @@
 const BASE_URL = "http://localhost/veblog/";
 function redirect($url)
 {
-    header("location: " . BASE_URL . $url);
+    header("location: " . trim(BASE_URL, '/') . '/' . trim($url, '/'));
     exit;
 };
-redirect("functions/pdo_connection.php");
+function assets($file)
+{
+    return trim(BASE_URL, '/' . '/' . trim($file, '/'));
+}
+function url($url)
+{
+    return trim(BASE_URL, '/' . '/' . trim($url, '/')); // یو آر ال مارو جایی نمیبره برعکس ریدایرکت این برای مثلا کلیک کردن روی دکمه هاس
+    // که میخوام مثلا برم توی صفحه پست
+}
+function dd($var)
+{ // dump and die=> we use the var_dump in the normal this is the var_dump and die the program
+    var_dump($var);
+    exit;
+}
